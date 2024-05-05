eab6ddb4dd38840f8a51250a67b4263ee0287f35
rasel.mahmud <rasel.mahmud@bracits.com>
2024-05-05 17:07:20 +0600

    Adding navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { RandomStringComponent } from './random-string/random-string.component';
 import { ObjectTestingComponent } from './object-testing/object-testing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RandomStringComponent, ObjectTestingComponent],
+  imports: [RouterOutlet, RandomStringComponent, ObjectTestingComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
