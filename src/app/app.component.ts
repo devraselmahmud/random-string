ce13376709c123c240f0626e530d849ca2031e8e
rasel.mahmud <rasel.mahmud@bracits.com>
2024-05-05 18:05:38 +0600

    set active link color as green

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,40 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { RandomStringComponent } from './random-string/random-string.component';
 import { ObjectTestingComponent } from './object-testing/object-testing.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RandomStringComponent, ObjectTestingComponent, RouterLink],
+  imports: [RouterOutlet, RandomStringComponent, ObjectTestingComponent, RouterLink, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = 'random-project';
+
+  homeColor: string = 'black';
+  randomColor: string = 'blue';
+  studentColor: string = 'blue';
+
+  changeColor(event: MouseEvent){
+    const target = event.target as HTMLLinkElement;
+
+    if(target.textContent === 'Home'){
+      this.homeColor = 'green';
+      this.randomColor = 'blue';
+      this.studentColor = 'blue';
+    }
+    else if(target.textContent === 'Random'){
+      this.randomColor = 'green';
+      this.homeColor = 'black';
+      this.studentColor = 'blue';
+    }else if(target.textContent === 'Student'){
+      this.studentColor = 'green';
+      this.homeColor = 'black';
+      this.randomColor = 'blue';
+    }
+  }
+
 }
